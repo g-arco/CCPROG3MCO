6c23e8ef1fd9a26686cc0d764b104b9c914437e8
g-arco <gabrielle_mae_arco@gmail.com>
2023-07-28 15:36:54 +0800

    loading frame and new money frame for maintenance commit

--- src/LoadingFrame.java
+++ src/LoadingFrame.java
@@ -1,21 +1,43 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class LoadingFrame extends JFrame {
 
-    public LoadingFrame(){
+        public LoadingFrame(){
 
-        JFrame load = new JFrame("Processing...");
+            JFrame load = new JFrame("Processing...");
 
-        load.setSize(300, 200);
-        load.setLayout(null);
-        load.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            load.setSize(480, 480);
+            load.setLayout(null);
+            load.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        ImageIcon gifHaloHalo = new ImageIcon();
-        JLabel loadHaloHalo = new JLabel(gifHaloHalo);
+            ImageIcon gifHaloHalo = new ImageIcon(getClass().getResource("/resources/halohalo_load.gif"));
+            JLabel loadHaloHalo = new JLabel(gifHaloHalo);
+            loadHaloHalo.setBounds(0, 0, 480, 480);
+
+            String str = "Dispensing...";
+            JLabel strOutput = new JLabel(str);//get string from model
+            strOutput.setBounds(190, 50, 100, 60);
+            strOutput.setFont(new Font("Comic Sans MS",Font.PLAIN,15));
+            strOutput.setForeground(Color.red);
+
+            loadHaloHalo.add(strOutput);
+            load.add(loadHaloHalo);
 
 
+            load.setVisible(true);
 
-        load.setVisible(true);
+        }
 
+        public static void main(String args[])
+        {
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new LoadingFrame().setVisible(true);
+                }
+            });
+
+        }
     }
-}
+
---
+++ src/MoneyMaintenanceFrame.java
@@ -0,0 +1,6 @@
+public class MoneyMaintenanceFrame {
+
+    public MoneyMaintenanceFrame(){
+
+    }
+}
